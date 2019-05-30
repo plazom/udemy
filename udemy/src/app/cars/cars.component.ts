d549f780a3d74dac4d8e5038038e5518d692e922
Maks <plazom@gmail.com>
2019-05-30 17:24:48 +0200

    lesson 13

--- udemy/src/app/cars/cars.component.ts
+++ udemy/src/app/cars/cars.component.ts
@@ -9,6 +9,7 @@ export class CarsComponent implements OnInit {
   carName: string = '';
   addCarStatus: boolean = false;
   cars: Array<string> = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
+  dates:Array<Date> = [new Date(2015, 10, 5), new Date(2011, 3, 10), new Date(2017, 7, 14), new Date(2000, 9, 4)]
   ngOnInit() {
   }
 
@@ -16,8 +17,5 @@ export class CarsComponent implements OnInit {
     this.addCarStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-  setBigCarText(car: string): boolean {
-    return car.length > 4 ? true : false;
   }
 }
