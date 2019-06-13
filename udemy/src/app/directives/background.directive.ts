87e372815b41ea84dc75f141b6441edcc687ea7e
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-06-13 11:43:01 +0200

    lesson 22 - HostBinding, HostListener inside directive

--- udemy/src/app/directives/background.directive.ts
+++ udemy/src/app/directives/background.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive(
     {
@@ -6,11 +6,24 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
     }
 )
 export class BackgroundDirective implements OnInit {
-    constructor(private element:ElementRef, private renderer:Renderer2) { }
-    ngOnInit(){
-        const {nativeElement} = this.element;
-        this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-        this.renderer.addClass(nativeElement, 'white-text');
-        //this.element.nativeElement.style.backgroundColor = 'blue'; // не писать так!!!
+    @HostBinding('style.backgroundColor') bacground: string;
+    
+    constructor(private element: ElementRef, private renderer: Renderer2) { }
+    ngOnInit() {
     }
+
+    @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
+        // const {nativeElement} = this.element;
+        // this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+        // this.renderer.addClass(nativeElement, 'white-text');
+        this.bacground = 'red';
+    }
+
+    @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
+        // const {nativeElement} = this.element;
+        // this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+        // this.renderer.removeClass(nativeElement, 'white-text');
+        this.bacground = 'transparent';
+    }
+
 }
