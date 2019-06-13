44d49f7cacab55e3205bbf65b392c5d728f03ca1
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-06-13 10:41:58 +0200

    lesson 20 - directive

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 import { AddCarComponent } from './add-car/add-car.component';
+import { BackgroundDirective } from './directives/background.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
     CarComponent,
-    AddCarComponent
+    AddCarComponent,
+    BackgroundDirective
   ],
   imports: [
     BrowserModule,
