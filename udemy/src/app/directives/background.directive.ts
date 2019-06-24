d3ff6eff4268b90809a2a9c723a4223845450bf3
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-06-24 17:02:35 +0200

    lesson 23

--- udemy/src/app/directives/background.directive.ts
+++ udemy/src/app/directives/background.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive(
     {
@@ -6,24 +6,27 @@ import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } f
     }
 )
 export class BackgroundDirective implements OnInit {
+    @Input('appBackground') hoverColor:string = 'green';
+    @Input() defaultColor:string = 'transparent';
     @HostBinding('style.backgroundColor') bacground: string;
-    
+
     constructor(private element: ElementRef, private renderer: Renderer2) { }
     ngOnInit() {
+        this.bacground = this.defaultColor;
     }
 
     @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
         // const {nativeElement} = this.element;
         // this.renderer.setStyle(nativeElement, 'background-color', 'blue');
         // this.renderer.addClass(nativeElement, 'white-text');
-        this.bacground = 'red';
+        this.bacground = this.hoverColor;
     }
 
     @HostListener('mouseleave', ['$event']) mouseLeave(event: Event) {
         // const {nativeElement} = this.element;
         // this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
         // this.renderer.removeClass(nativeElement, 'white-text');
-        this.bacground = 'transparent';
+        this.bacground = this.defaultColor;
     }
 
 }
