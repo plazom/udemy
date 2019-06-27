19ace6846d6d5742ec1fcfd4b940bd8e90c9fb96
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-06-27 13:58:58 +0200

    lesson 25 - standard pipes

--- udemy/src/app/app.component.ts
+++ udemy/src/app/app.component.ts
@@ -7,11 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   //title = 'udemy';
-  headerText = "Создание своей диррективы.";
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-  onClick(number:number)
-  {
-    this.current = number;
+
+  // headerText = "Создание своей диррективы.";
+  // items = [1, 2, 3, 4, 5];
+  // current = 1;
+  // onClick(number:number)
+  // {
+  //   this.current = number;
+  // }
+
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+    {
+      xyz: 3,
+      numbers: [1, 2, 3]
+    }
   }
+
 }
