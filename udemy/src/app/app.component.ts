e6c65f18226743c3dc69437d027fc2149b6ea032
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-01 14:24:02 +0200

    lesson 43 - created own Validator in Reactive form

--- udemy/src/app/app.component.ts
+++ udemy/src/app/app.component.ts
@@ -14,13 +14,15 @@ export class AppComponent implements OnInit {
     text: 'Нет'
   }];
 
+  charsCount = 5;
+
   form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
       user: new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        pass: new FormControl('', Validators.required),
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
       }),
       country: new FormControl('ru'),
       answer: new FormControl('no')
@@ -29,5 +31,14 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log('Submited!', this.form);
+  }
+
+  checkForLength(control: FormControl) {
+    if (control.value.length <= this.charsCount) {
+      return {
+        'lengthError': true
+      };
+    }
+    return null;
   }
 }
