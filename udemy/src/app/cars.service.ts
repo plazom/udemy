068210ae34280363b938fd413f0ab4cc4e671424
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-03 15:23:16 +0200

    lesson51 - HTTP added HEADERS

--- udemy/src/app/cars.service.ts
+++ udemy/src/app/cars.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 
 @Injectable()
 export class CarsService {
   constructor(private http: Http) {}
 
   getCars() {
-    return this.http.get('http://localhost:3000/cars')
+    const headers = new Headers({
+      'Content-Type': 'application/json; charset=utf8'
+    });
+    return this.http.get('http://localhost:3000/cars', {
+      headers: headers
+    })
       .map((response: Response) => response.json());
   }
 
@@ -15,7 +20,12 @@ export class CarsService {
       name: carName,
       color: 'blue'
     };
-    return this.http.post('http://localhost:3000/cars', data)
+    const headers = new Headers({
+      'Content-Type': 'application/json; charset=utf8'
+    }); 
+    return this.http.post('http://localhost:3000/cars', data, {
+      headers
+    })
       .map((response: Response) => response.json());
   }
 
