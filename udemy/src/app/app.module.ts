6e52cae2a998be4df915ebbba270be0ae032f4e2
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-04 12:03:33 +0200

    lesson 54 - created simple router

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarsService } from './cars.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'cars', component: CarsPageComponent },
+  { path: '', component: HomePageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +23,8 @@ import { CarsService } from './cars.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
