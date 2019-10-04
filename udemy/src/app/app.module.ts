b4912308eb8e19845c62b8299e7960b3a69236ae
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-04 12:01:00 +0200

    lesson 54 - init components witout routing

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarsService } from './cars.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CarsPageComponent
   ],
   imports: [
     BrowserModule,
