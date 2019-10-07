cd5c5d890b4eec059ec8aea62d3e48f15a8aec95
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-07 16:25:47 +0200

    lesson 60 - children in routers. Routing inside of the routing

--- udemy/src/app/app-routing.module.ts
+++ udemy/src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
 
 const appRoutes: Routes = [
-  { path: 'cars', component: CarsPageComponent },
-  { path: 'cars/:id/:name', component: CarPageComponent },
+  { path: 'cars', component: CarsPageComponent, children: [
+    { path: ':id/:name', component: CarPageComponent }
+  ] },
   { path: '', component: HomePageComponent }
 ];
 
