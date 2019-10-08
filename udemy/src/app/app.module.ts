980df186265ee87c3e8cdadeed06f849063a1226
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-08 14:17:45 +0200

    lesson67, init animation code. npm i @angular/animations. import BrowserAnimationsModule

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -4,23 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './header/header.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    AppRoutingModule,
-    SharedModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
