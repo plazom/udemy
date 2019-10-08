1ebce36c52f3bf079d70046b6efec496257743a0
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-08 14:41:21 +0200

    lesson67, created simple base animation

--- udemy/src/app/app.component.ts
+++ udemy/src/app/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('clickedDiv', [
+      state('start', style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px'
+      })),
+      state('end', style({
+        backgroundColor: 'red',
+        width: '300px',
+        height: '300px'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate('800ms 0.5s ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
+  clickedDivState = 'start';
+
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+      this.clickedDivState = 'start';
+    }, 3000);
+  }
 }
