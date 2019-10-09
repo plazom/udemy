66dbe0263c7eed4e762f307b75846863b93dd3fc
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-09 17:26:15 +0200

    lesson78, testing of ASYNC methods of service async, spyOn, fixture.whenStable()

--- udemy/src/app/car/car.component.spec.ts
+++ udemy/src/app/car/car.component.spec.ts
@@ -1,11 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CarComponent } from './car.component';
 import { CarService } from './car.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 describe('CarComponent', () => {
 
   let fixture: ComponentFixture<CarComponent>;
   let component: CarComponent;
+  let carService: CarService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,6 +17,7 @@ describe('CarComponent', () => {
 
     fixture = TestBed.createComponent(CarComponent);
     component = fixture.debugElement.componentInstance;
+    carService = fixture.debugElement.injector.get(CarService);
   });
 
   it('should create component instance', () => {
@@ -28,13 +32,11 @@ describe('CarComponent', () => {
   });
 
   it(`should inject CarService`, () => {
-    const carService = fixture.debugElement.injector.get(CarService);
     fixture.detectChanges();
     expect(component.isCarVisible).toEqual(carService.getVisibility());
   });
 
   it(`should display car if is visible`, () => {
-    const carService = fixture.debugElement.injector.get(CarService);
     carService.showCar();
     fixture.detectChanges();
     const nativeEl = fixture.debugElement.nativeElement;
@@ -43,12 +45,27 @@ describe('CarComponent', () => {
   });
 
   it(`shouldn't display car if isn't visible`, () => {
-    const carService = fixture.debugElement.injector.get(CarService);
     carService.hideCar();
     fixture.detectChanges();
     const nativeEl = fixture.debugElement.nativeElement;
     const text = nativeEl.querySelector('span').textContent;
     expect(text).not.toEqual('Car is visible');
   });
+
+  it(`shouldn't get car name if not async`, () => {
+    spyOn(carService, 'getCarName')
+      .and.returnValue(Observable.of('TestCar').delay(100));
+    fixture.detectChanges();
+    expect(component.carName).toBe(undefined);
+  });
+
+  it(`should get car name if async`, async(() => {
+    spyOn(carService, 'getCarName')
+      .and.returnValue(Observable.of('TestCar').delay(100));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.carName).toEqual('TestCar');
+    });
+  }));
 
 });
