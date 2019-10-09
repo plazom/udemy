76f05566575341fb1882eb77c0fd9e999f51f3cc
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-09 16:49:33 +0200

    lesson76, testing of service
    const carService = fixture.debugElement.injector.get(CarService);

---
+++ udemy/src/app/car/car.service.ts
@@ -0,0 +1,16 @@
+export class CarService {
+  private isVisible = true;
+
+  showCar() {
+    this.isVisible = true;
+  }
+
+  hideCar() {
+    this.isVisible = false;
+  }
+
+  getVisibility() {
+    return this.isVisible;
+  }
+
+}
