5a49748a80db0ad69a316a8d0e58b32bb59df890
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-09 13:59:46 +0200

    lesson73, EVENTS in animation (start, done).(@divTrigger.start)="onAnimationStart($event)", (@divTrigger.done)="onAnimationDone($event)"

--- udemy/src/app/app.component.ts
+++ udemy/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { changeWidthTrigger, divTrigger } from './app.animations';
+import { AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import { changeWidthTrigger, divTrigger } from './app.animations';
 })
 export class AppComponent {
   isVisible = false;
+
+  onAnimationStart(event: AnimationEvent) {
+    console.log('Start: ', event);
+  }
+
+  onAnimationDone(event: AnimationEvent) {
+    console.log('Done: ', event);
+  }
 }
