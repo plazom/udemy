a76c4cfeee5a34ca39fbacb3e3e805468c6e86dc
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-09 11:55:15 +0200

    lesson70, animations in separate file. standard state VOID

--- udemy/src/app/app.component.ts
+++ udemy/src/app/app.component.ts
@@ -1,71 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
+import { divTrigger } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        width: '170px',
-        height: '170px',
-        backgroundColor: 'orange'
-      })),
-      transition('start <=> end', animate('800ms ease-in')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(400))
-    ]),
-    trigger('multi', [
-      state('start', style({
-        width: '150px',
-        height: '150px',
-        border: '5px solid black'
-      })),
-      state('end', style({
-        width: '170px',
-        height: '170px',
-        background: 'blue'
-      })),
-      transition('start <=> end', [
-        style({
-          background: 'red'
-        }),
-        animate(1500, style({
-          background: 'yellow'
-        })),
-        animate(1000, style({
-          width: '200px',
-          height: '200px'
-        })),
-        animate(1000)
-      ])
-    ])
-  ]
+  animations: [divTrigger]
 })
 export class AppComponent {
-  clickedDivState = 'start';
-  multiState = 'start';
-
-  changeDivState() {
-    this.clickedDivState = 'end';
-    setTimeout(() => {
-      this.clickedDivState = 'start';
-    }, 3000);
-  }
+  isVisible = false;
 }
