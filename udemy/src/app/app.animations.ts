b3b28b88f0fff7e505fe61992c87fd2cb229c02e
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-09 12:12:22 +0200

    lesson71, void => * is :enter . * => void is :leave . height/width: '*' - nachalnaja vysota/sirina

--- udemy/src/app/app.animations.ts
+++ udemy/src/app/app.animations.ts
@@ -7,10 +7,8 @@ import {
 } from '@angular/animations';
 
 export const divTrigger = trigger('divTrigger', [
-  state('show', style({
-
-  })),
-  transition('void => show', [
+  // void => *
+  transition(':enter', [
     style({
       opacity: 0
     }),
@@ -18,7 +16,19 @@ export const divTrigger = trigger('divTrigger', [
       opacity: 1
     }))
   ]),
-  transition('show => void', animate(500, style({
+  // * => void
+  transition(':leave', animate(500, style({
     opacity: 0
   })))
+]);
+
+export const changeWidthTrigger = trigger('changeWidth', [
+  transition('* => *', [
+    animate(1000, style({
+      height: '10px'
+    })),
+    animate(1000, style({
+      height: '*'
+    }))
+  ])
 ]);
