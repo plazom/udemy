4220057a103a25edb82b10c009d267171296b9cf
Maksym Maksymyshyn <plazom@gmail.com>
2019-10-12 17:29:12 +0200

    lesson 129 - cycle like 'let item of array', array.forEach

--- es6/index.js
+++ es6/index.js
@@ -1,15 +1,17 @@
-let name = 'WFM';
+let array = [1, 2, 3, 4, 5];
 
-//let str = 'Hello ' + name + ', glad to \'see\' you!';
-//let str = `Hello ${name}, glad to "see" you! ${5 + 10}`;
+//for (let i = 0; i < array.length; i++) {
+//    console.log(array[i]);
+//}
 
-let html = `
-    <div>
-        <h1>${name}</h1>
-        <span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, tempore.</p>
-        </span>
-    </div>
-`;
+//array.forEach(function (item) {
+//    console.log(item);
+//});
 
-console.log(html);
+//for (let item of array) {
+//    console.log('Item: ', item);
+//}
+
+for (let item of 'ABCDEFG') {
+    console.log('Item: ', item);
+}
