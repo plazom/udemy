b12bbaa8377b60f7dceb0ae87ba9455d86908ff1
Maksym Maksymyshyn <plazom@gmail.com>
2019-10-12 17:50:33 +0200

    lesson 136 - new metchods: Object.assign, array.find, str.repeat, str.startWith, str.includes

--- es6/index.js
+++ es6/index.js
@@ -1,15 +1,17 @@
-let fib = {
-    *[Symbol.iterator]() {
-        let cur = 1, pre = 0;
-        for (;;) {
-            [cur, pre] = [cur + pre, cur];
-            yield cur;
-        }
-    }
-};
+// let obj1 = {a: 1};
+// let obj2 = {b: 2, c: 3};
+//
+// let obj3 = Object.assign({d: 4}, obj1, obj2);
+//
+// console.log('Obj1', obj1);
+// console.log('Obj2', obj2);
+// console.log('Obj3', obj3);
 
+// let findedItem = [1, 2, 3, 4].find(x => x > 2);
+// console.log(findedItem);
 
-for (let n of fib) {
-    if (n > 3000) break;
-    console.log(n);
-}
+let str = 'Hello!';
+
+console.log('Repeat: ', str.repeat(3));
+console.log('StartsWith: ', str.startsWith('el', 1));
+console.log('Includes: ', str.includes('llo', 2));
