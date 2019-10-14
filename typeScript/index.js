d5afa04b5008aa3ab87277356c79cd4c61839941
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-14 17:22:53 +0200

    lesson150 - namespace

--- typeScript/index.js
+++ typeScript/index.js
@@ -1,43 +1,19 @@
 "use strict";
-// function logger(constrFn: Function) {
-//     console.log(constrFn);
-// }
-//
-// function shouldLog(flag: boolean): any {
-//     return flag ? logger : null;
-// }
-//
-// @shouldLog(true)
-// class User {
-//     constructor(public name: string, public age: number) {
-//         console.log('I am  new user');
-//     }
-// }
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-// ===========================================
-function addShowAbility(constructorFn) {
-    constructorFn.prototype.showHtml = function () {
-        var pre = document.createElement('pre');
-        pre.innerHTML = JSON.stringify(this);
-        document.body.appendChild(pre);
-    };
-}
-var User = (function () {
-    function User(name, age, job) {
-        this.name = name;
-        this.age = age;
-        this.job = job;
+var Util;
+(function (Util) {
+    function isEmpty(d) {
+        return !d;
     }
-    return User;
-}());
-User = __decorate([
-    addShowAbility
-], User);
-var user = new User('WFM', 20, 'Frontend');
-console.log(user);
-user.showHtml();
+    Util.isEmpty = isEmpty;
+    function isUndefined(d) {
+        return typeof d === 'undefined';
+    }
+    Util.PI = Math.PI;
+    Util.EXP = Math.E;
+})(Util || (Util = {}));
+var EXP = 'TEST';
+console.log(Util.isEmpty(''));
+console.log(Util.isEmpty('dsa'));
+console.log(Util.PI);
+console.log(EXP);
+console.log(Util.EXP);
--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,33 +1,20 @@
-// function logger(constrFn: Function) {
-//     console.log(constrFn);
-// }
-//
-// function shouldLog(flag: boolean): any {
-//     return flag ? logger : null;
-// }
-//
-// @shouldLog(true)
-// class User {
-//     constructor(public name: string, public age: number) {
-//         console.log('I am  new user');
-//     }
-// }
-
-// ===========================================
-
-function addShowAbility(constructorFn: Function) {
-    constructorFn.prototype.showHtml = function() {
-        const pre = document.createElement('pre');
-        pre.innerHTML = JSON.stringify(this);
-        document.body.appendChild(pre);
+namespace Util {
+    export function isEmpty(d: any): boolean {
+        return !d;
     }
+
+    function isUndefined(d: any): boolean {
+        return typeof d === 'undefined';
+    }
+
+    export const PI = Math.PI;
+    export const EXP = Math.E;
 }
 
-@addShowAbility
-class User {
-    constructor(public name: string, public age: number, public job: string) {}
-}
+const EXP = 'TEST';
 
-let user = new User('WFM', 20, 'Frontend');
-console.log(user);
-(<any>user).showHtml();
+console.log(Util.isEmpty(''));
+console.log(Util.isEmpty('dsa'));
+console.log(Util.PI);
+console.log(EXP);
+console.log(Util.EXP);
