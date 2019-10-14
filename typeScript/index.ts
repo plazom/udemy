3b8df17db2237b6d5d96bd462b81e602507b8837
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-14 10:52:50 +0200

    lesson140 - Arrays

---
+++ typeScript/index.ts
@@ -0,0 +1,14 @@
+let numArray1: number[] = [1, 2, 3];
+let numArray2: Array<number> = [1, 2, 3];
+
+let strArray1: string[] = ['1', '2', '3'];
+let strArray2: Array<string> = ['1', '2', '3'];
+
+let boolArray1: boolean[] = [true, false];
+let boolArray2: Array<boolean> = [true, false];
+
+// tuples
+let array: [number, number, string] = [1, 2, '3'];
+
+let array2: [boolean, string, number] = [true, 'str', 22];
+
