b00f74037e2d7a65ebb22e6f1f104f673829b76b
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-18 12:40:00 +0200

    lesson155 - webpack config

--- WebPack/init.js
+++
@@ -1,3 +0,0 @@
-import * as $ from 'jquery';
-
-$('body').html('Webpack init with npm!');
---
+++ WebPack/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+
+module.exports = {
+
+    context: path.resolve(__dirname, 'src'),
+
+    entry: './init.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    resolve: {
+        extensions: ['.js']
+    },
+
+    watch: false
+
+};
