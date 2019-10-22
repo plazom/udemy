78d79b449fb617043d4d61e2b529b62496dbc0da
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-22 17:52:40 +0200

    lesson177, first, last, find, take(10), skip(2), skipWhile, takeWhile, skipUntil(timer), takeUntil(timer)

--- RxJS/index.js
+++ RxJS/index.js
@@ -12,7 +12,12 @@ function createSubscribe(name) {
     }
 }
 
-Rx.Observable.fromEvent(document.querySelector('input'), 'keyup')
-    //.map(x => x.target.value)
-    .pluck('target', 'value')
-    .subscribe(createSubscribe('map'));
+//Rx.Observable.interval(500)
+//    .skipWhile(x => x < 5)
+//    .takeWhile(x => x < 13)
+//    .subscribe(createSubscribe('takeWhile'));
+
+Rx.Observable.interval(500)
+    .skipUntil(Rx.Observable.timer(3000))
+    .takeUntil(Rx.Observable.timer(5000))
+    .subscribe(createSubscribe('skipUntil'));
