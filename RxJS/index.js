98892833fba0e4d907ebfcbafb01bebe9585f78f
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-23 16:21:32 +0200

    lesson181 - merge, mergeAll, concat, concatAll

--- RxJS/index.js
+++ RxJS/index.js
@@ -7,13 +7,26 @@ function createSubscribe(name) {
             console.log('Error: ', x);
         },
         complete() {
-            console.log(name, ' completed');
+            console.log(name, ': Completed.');
         }
     }
 }
 
+// const s1$ = Rx.Observable.interval(1000).map(x => 'Stream 1: ' + x);
+// const s2$ = Rx.Observable.interval(500).map(x => 'Stream 2: ' + x);
+//
+// Rx.Observable
+//     .merge(s1$, s2$)
+//     .take(12)
+//     .subscribe(createSubscribe('merge'));
+
+// Rx.Observable.range(1, 2) // 1, 2, 3
+//     .map(x => Rx.Observable.range(1, 3))
+//     .mergeAll()
+//     .subscribe(createSubscribe('mergeAll'));
+
 
 Rx.Observable.range(1, 3)
-    .map(x => x + 1)
-    .let(observer => observer.map(x => x * x))
-    .subscribe(createSubscribe('let'));
+    .map(x => Rx.Observable.range(x, 3))
+    .concatAll()
+    .subscribe(createSubscribe('concatAll'));
