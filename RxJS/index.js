0a5cb5b2c738da7687a6d94f7888ba169b1a6112
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-23 15:41:20 +0200

    lesson179, Buffer, BufferTime, BufferCount

--- RxJS/index.js
+++ RxJS/index.js
@@ -12,11 +12,8 @@ function createSubscribe(name) {
     }
 }
 
-//Rx.Observable.fromEvent(document.querySelector('input'), 'keyup')
-//    .map(e => e.target.value)
-//    .distinct()
-//    .subscribe(createSubscribe('debounceTime'));
 
-Rx.Observable.from([1, 2, 3, 3, 3, 5, 5, 1, 1, 99, 99, 2, 4, 6])
-    .distinctUntilChanged()
-    subscribe(createSubscribe('from'))
+Rx.Observable.interval(1000)
+    .buffer(Rx.Observable.fromEvent(document, 'click'))
+    .map(x => x.length)
+    .subscribe(createSubscribe('buffer'));
