772ab522fae4e19a9bd4219b2bb0fb64c5546f4c
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-24 13:43:43 +0200

    lesson186 - get User from VK. Practice.

--- RxJS/index.js
+++ RxJS/index.js
@@ -1,23 +1,29 @@
-function createSubscribe(name) {
-    return {
-        next(x) {
-            console.log(name, ': ', x);
-        },
-        error(err) {
-            console.log(name, ': ', x);
-        },
-        complete() {
-            console.log(name, ': Completed.');
-        }
-    }
+function getUserById(id) {
+    const params = {
+        access_token: '547a815314348f7693aa5d246e7f2dfc06326e659082d5b0498edc6f96c7fa390ab547e02b4ce6fde65f6',
+        user_ids: id,
+        fields: 'photo_big'
+    };
+
+    return $.ajax({
+        url: 'https://api.vk.com/method/users.get?' + $.param(params),
+        type: 'GET',
+        dataType: 'JSONP'
+    }).promise();
 }
 
-const subject$ = new Rx.AsyncSubject();
-
-
-subject$.next(1);
-subject$.next('WFM');
-subject$.complete();
-
-subject$.subscribe(createSubscribe('async'));
-
+Rx.Observable.fromEvent($('input'), 'keyup')
+    .pluck('target', 'value')
+    .distinct()
+    .debounceTime(2000)
+    .mergeMap(v => Rx.Observable.fromPromise(getUserById(v)))
+    .catch(error => Rx.Observable.of(error))
+    .map(x => x.response[0])
+    .subscribe(
+        (user) => {
+            $('h1').html(`${user.first_name} ${user.last_name} <i>${user.uid}</i>`);
+            $('img').attr('src', user.photo_big);
+        },
+        error => console.log(error),
+        () => console.log('Completed')
+    );
