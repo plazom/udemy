869de8975e97177abdea7eee4b5f72885a8897b1
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-24 10:24:54 +0200

    lesson182 - MergeMap, ConcatMap

--- RxJS/index.js
+++ RxJS/index.js
@@ -7,26 +7,16 @@ function createSubscribe(name) {
             console.log('Error: ', x);
         },
         complete() {
-            console.log(name, ': Completed.');
+            console.log(name, ' completed');
         }
     }
 }
 
-// const s1$ = Rx.Observable.interval(1000).map(x => 'Stream 1: ' + x);
-// const s2$ = Rx.Observable.interval(500).map(x => 'Stream 2: ' + x);
-//
-// Rx.Observable
-//     .merge(s1$, s2$)
-//     .take(12)
-//     .subscribe(createSubscribe('merge'));
-
-// Rx.Observable.range(1, 2) // 1, 2, 3
-//     .map(x => Rx.Observable.range(1, 3))
-//     .mergeAll()
-//     .subscribe(createSubscribe('mergeAll'));
-
 
 Rx.Observable.range(1, 3)
-    .map(x => Rx.Observable.range(x, 3))
-    .concatAll()
-    .subscribe(createSubscribe('concatAll'));
+    .concatMap((x, i) => {
+        return Rx.Observable.interval(100)
+            .take(x)
+            .map(q => i)
+    })
+    .subscribe(createSubscribe('concatMap'))
