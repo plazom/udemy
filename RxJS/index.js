8ec05397ebd7eb6f7f94f5d929c0fdbf7cbddaaa
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-24 11:20:06 +0200

    lesson183 - zip, CombineLatest

--- RxJS/index.js
+++ RxJS/index.js
@@ -7,16 +7,19 @@ function createSubscribe(name) {
             console.log('Error: ', x);
         },
         complete() {
-            console.log(name, ' completed');
+            console.log(name, ': Completed.');
         }
     }
 }
 
 
-Rx.Observable.range(1, 3)
-    .concatMap((x, i) => {
-        return Rx.Observable.interval(100)
-            .take(x)
-            .map(q => i)
-    })
-    .subscribe(createSubscribe('concatMap'))
+const t1$ = Rx.Observable.timer(1000, 2000);
+const t2$ = Rx.Observable.timer(2000, 2000);
+const t3$ = Rx.Observable.timer(3000, 2000);
+const t4$ = Rx.Observable.timer(4000, 2000);
+
+Rx.Observable
+    .combineLatest(t1$, t2$, t3$, t4$)
+    .take(10)
+    .subscribe(createSubscribe('combineLatest'));
+
