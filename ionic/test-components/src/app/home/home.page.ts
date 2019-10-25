f7412a6e423b8679bed63f7198488609ee954a8f
Maksym Maksymyshyn <mmaksymyshyn@dxc.com>
2019-10-25 15:41:39 +0200

    lesson9 - component Action sheet

--- ionic/test-components/src/app/home/home.page.ts
+++ ionic/test-components/src/app/home/home.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import {ActionSheetController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: 'action-sheet.component.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(public actionSheetCtrl:ActionSheetController) {}
+	async showActionSheet(){
+		const actionSheet = await this.actionSheetCtrl.create({
+			  header: 'Albums',
+			  buttons: [{
+				text: 'Delete',
+				role: 'destructive',
+				icon: 'happy',
+				handler: () => {
+				  console.log('Delete clicked');
+				}
+			  }, {
+				text: 'Share',
+				icon: 'share',
+				handler: () => {
+				  console.log('Share clicked');
+				}
+			  }, {
+				text: 'Play (open modal)',
+				icon: 'arrow-dropright-circle',
+				handler: () => {
+				  console.log('Play clicked');
+				}
+			  }, {
+				text: 'Favorite',
+				icon: 'heart',
+				handler: () => {
+				  console.log('Favorite clicked');
+				}
+			  }, {
+				text: 'Cancel',
+				icon: 'close',
+				role: 'cancel',
+				handler: () => {
+				  console.log('Cancel clicked');
+				}
+			  }]
+			});
+		await actionSheet.present();
+	}
 }
